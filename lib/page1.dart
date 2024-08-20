b1c13609b75591c09ded7bda73cd7613569a040f
wava lutfiah <wavalutfiahmarhas@gmail.com>
2024-08-20 12:09:24 +0700

    commit pertama

---
+++ lib/page1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatelessWidget {
+  const page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/page2');
+          },
+          child:Text('pindah ke halaman 2')
+        ),
+        ),
+      );
+    
+  }
+}
---
+++ lib/page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ini halaman 2'),
+      ),
+    );
+  }
+}
