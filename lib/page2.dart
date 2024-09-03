612db43ac5866340222059b67f6b8b883f3cb100
wava lutfiah <wavalutfiahmarhas@gmail.com>
2024-09-03 12:37:34 +0700

    merubah codingan page2 dart

--- lib/page2.dart
+++ lib/page2.dart
@@ -6,8 +6,33 @@ class page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ini halaman 2'),
+      body: Row(children: [
+        Container(
+          height: 100,
+          width: 100,
+          color: Colors.red,  
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,  
+            ),
+          Row(children: [
+            Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.yellow,
+            )
+
+          ],)
+          
+          
+      ]
       ),
     );
   }
